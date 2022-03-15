feff799920071caa469c293efc05851c606b277a
Caitlin Swickard <caitlin6029@gmail.com>
2022-03-15 16:50:02 -0600

    redid fetch with async and await

--- script.js
+++ script.js
@@ -5,18 +5,34 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke();
 
-function generateJoke() {
+// using async and await for API call
+async function generateJoke() {
   const config = {
     headers: {
       'Accept': 'application/json'
     }
   }
+  const res = await fetch('https://icanhazdadjoke.com', config)
 
-  // this API requires headers: accept which is the config we created
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke
-    })
-
+  const data = await res.json()
+  jokeEl.innerHTML = data.joke
 }
+
+
+
+// this is using the .then 
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   }
+
+//   // this API requires headers: accept which is the config we created
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+
+// }
